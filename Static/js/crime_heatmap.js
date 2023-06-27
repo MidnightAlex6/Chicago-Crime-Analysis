f2f7a4297a1549af8ef5f57644b6b472521d207b
Alex Valerio <126301312+MidnightAlex6@users.noreply.github.com>
2023-06-27 02:47:18 -0500

    updated code so it has bar chart and crime heat map

--- Static/js/crime_heatmap.js
+++ Static/js/crime_heatmap.js
@@ -1,14 +1,12 @@
 let heatLayer; // Declare a variable to store the heatmap layer
-
 function init() {
   // This checks that our initial function runs.
   console.log("The Init() function ran");
-
   // Create dropdown/select
   d3.json("/api/v1.0/crime_heatmap_dropdown")
     .then(i => {
       // Get the dropdown/select element
-      let dropdownMenu = d3.select("#selDataset")
+      let dropdownMenu = d3.select("#selDataset1")
         .selectAll("option")
         .data(i)
         .enter()
@@ -19,38 +17,52 @@ function init() {
     .catch(error => {
       console.error("Error retrieving dropdown data:", error);
     });
-
+  // create dropdown/select
+  d3.json("/api/v1.0/dropdown").then(i => {
+    // Get the dropdown/select element
+    let dropdownMenu2 = d3.select("#selDataset2")
+    .selectAll("option")
+    .data(i)
+    .enter()
+    .append("option")
+    .attr("value", d => d)
+    .text(d =>  d);
+  });
+  // run functions to generate plots with default location = "STREET"
+  createBar('ABANDONED BUILDING');
   // loads arson heatmap
   heatmap("ARSON");
 }
-
 // Function that runs whenever the dropdown is changed
-function optionChanged(newlocation) {
+function optionChanged1(newlocation) {
   console.log("Change", newlocation);
   heatmap(newlocation);
 }
-
+// function that runs whenever the dropdown is changed
+// this function is in the HTML and is called with an input called 'this.value'
+// that comes from the select element (dropdown)
+function optionChanged2(newlocation2){
+  // code that updates graphics
+  // one way is to recall each function
+  createBar(newlocation2)
+};
 // Call the init() function to start the initialization process
 init();
-
 let myMap = L.map("map", {
   center: [41.8781, -87.6298], // Set center to Chicago's coordinates
   zoom: 12,
 });
-
 // Adding the tile layer
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
 }).addTo(myMap);
-
 function heatmap(crime) {
   d3.json("/api/v1.0/chicago_crime_heatmap")
     .then(function (data) {
       let crimedata = data.filter(i => i.PrimaryType === crime);
       console.log(crimedata);
       let heatArray = [];
-
       crimedata.forEach(function (row) {
         let lat = row.Latitude;
         let lng = row.Longitude;
@@ -58,12 +70,10 @@ function heatmap(crime) {
           heatArray.push([lat, lng]);
         }
       });
-
       if (heatLayer) {
         // If a heatmap layer already exists, remove it from the map
         myMap.removeLayer(heatLayer);
       }
-
       heatLayer = L.heatLayer(heatArray, {
         radius: 20,
         blur: 20,
@@ -73,7 +83,6 @@ function heatmap(crime) {
       console.error("Error retrieving crime data:", error);
     });
 }
-
 // No changes in the getLocationCoordinates() function
 function getLocationCoordinates(locationString) {
   let regex = /\((-?\d+\.\d+),\s*(-?\d+\.\d+)\)/;
@@ -86,4 +95,34 @@ function getLocationCoordinates(locationString) {
     }
   }
   return null;
+}
+function createBar(location){
+  // code that makes bar chart at id='bar'
+    d3.json("/api/v1.0/barcharts").then(data => {
+    console.log(data)
+    // let test = d3.select("#selDataset").property("value")
+    let sampleData = data.filter(data => data.LocationDescription === location);
+    let labels = sampleData.map(data => data.PrimaryType)
+    let values = sampleData.map(data => data.counts)
+    let labels10 = labels.slice(0, 10);
+    let values10 = values.slice(0, 10);
+    let trace = {
+     x: labels10,
+     y: values10,
+     type: 'bar',
+    //  text: otu_labels,
+     orientation: 'v'
+   };
+    let plotData = [trace];
+    let layout ={margin: {
+     l: 100,
+     r: 150,
+     t: 0,
+     b: 150
+   }
+    };
+    Plotly.newPlot("bar", plotData,layout);
+ })
+  // checking to see if function is running
+  // console.log(`This function generates bar chart of ${id} `)
 }
--- app.py
+++ app.py
@@ -76,5 +76,46 @@ def others():
         all_crimes.append(crimes_dict)
     return jsonify(all_crimes)
 
+@app.route("/api/v1.0/dropdown")
+def names2():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query data
+    results = session.query(Crimes.LocationDescription).distinct(Crimes.LocationDescription).order_by(Crimes.LocationDescription.asc()).all()
+    
+    session.close()
+    
+    # Convert list of tuples into normal list
+    all_crimes = []
+    for LocationDescription in results:
+
+        all_crimes.append(LocationDescription[0])
+
+    return jsonify(all_crimes)
+
+@app.route("/api/v1.0/barcharts")
+def others2():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all suicides by country"""
+    # Query all suicide data
+    # results = session.query(Crimes.ID, Crimes.LocationDescription, Crimes.PrimaryType).all()
+    results = session.query(Crimes.LocationDescription, Crimes.PrimaryType, func.count(Crimes.PrimaryType)).group_by(Crimes.LocationDescription, Crimes.PrimaryType).order_by(Crimes.LocationDescription.asc(), func.count(Crimes.PrimaryType).desc()).all()
+    print(results)
+    session.close()
+    
+    # Convert list of tuples into normal list
+    all_crimes = []
+    for LocationDescription, PrimaryType, i in results:
+        crimes_dict = {}
+        crimes_dict["LocationDescription"] = LocationDescription
+        crimes_dict["PrimaryType"] = PrimaryType
+        crimes_dict["counts"] = i
+        all_crimes.append(crimes_dict)
+
+    return jsonify(all_crimes)
+
 if __name__ == '__main__':
     app.run(debug=True)

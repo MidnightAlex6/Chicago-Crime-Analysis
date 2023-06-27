2bcfbf52ba4e2d9199f1b79737f8fb3cfc4327dc
Alex Valerio <126301312+MidnightAlex6@users.noreply.github.com>
2023-06-26 21:05:18 -0500

    completed code for crime heatmap

--- Static/js/crime_heatmap.js
+++ Static/js/crime_heatmap.js
@@ -1,44 +1,89 @@
-let myMap = L.map("map", {
-    center: [41.8781, -87.6298], // Set center to Chicago's coordinates
-    zoom: 7
-  });
-  
-  // Adding the tile layer
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
-  }).addTo(myMap);
-  
-  let jsonUrl = "./static/data/Clear_Crimes_2023.json";
-  
-  d3.json(jsonUrl).then(function(data) {
-  
-    let heatArray = [];
-  
-    data.forEach(function(row) {
-      let locationString = row.Location;
-      let coordinates = getLocationCoordinates(locationString);
-      if (coordinates) {
-        heatArray.push(coordinates);
-      }
+let heatLayer; // Declare a variable to store the heatmap layer
+
+function init() {
+  // This checks that our initial function runs.
+  console.log("The Init() function ran");
+
+  // Create dropdown/select
+  d3.json("/api/v1.0/crime_heatmap_dropdown")
+    .then(i => {
+      // Get the dropdown/select element
+      let dropdownMenu = d3.select("#selDataset")
+        .selectAll("option")
+        .data(i)
+        .enter()
+        .append("option")
+        .attr("value", d => d)
+        .text(d => d);
+    })
+    .catch(error => {
+      console.error("Error retrieving dropdown data:", error);
     });
-  
-    let heat = L.heatLayer(heatArray, {
-      radius: 20,
-      blur: 35
-    }).addTo(myMap);
-  
-  });
-  
-  function getLocationCoordinates(locationString) {
-    let regex = /\((-?\d+\.\d+),\s*(-?\d+\.\d+)\)/;
-    let match = regex.exec(locationString);
-    if (match && match.length === 3) {
-      let lat = parseFloat(match[1]);
-      let lng = parseFloat(match[2]);
-      if (!isNaN(lat) && !isNaN(lng)) {
-        return [lat, lng];
+
+  // loads arson heatmap
+  heatmap("ARSON");
+}
+
+// Function that runs whenever the dropdown is changed
+function optionChanged(newlocation) {
+  console.log("Change", newlocation);
+  heatmap(newlocation);
+}
+
+// Call the init() function to start the initialization process
+init();
+
+let myMap = L.map("map", {
+  center: [41.8781, -87.6298], // Set center to Chicago's coordinates
+  zoom: 12,
+});
+
+// Adding the tile layer
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+}).addTo(myMap);
+
+function heatmap(crime) {
+  d3.json("/api/v1.0/chicago_crime_heatmap")
+    .then(function (data) {
+      let crimedata = data.filter(i => i.PrimaryType === crime);
+      console.log(crimedata);
+      let heatArray = [];
+
+      crimedata.forEach(function (row) {
+        let lat = row.Latitude;
+        let lng = row.Longitude;
+        if (!isNaN(lat) && !isNaN(lng)) {
+          heatArray.push([lat, lng]);
+        }
+      });
+
+      if (heatLayer) {
+        // If a heatmap layer already exists, remove it from the map
+        myMap.removeLayer(heatLayer);
       }
+
+      heatLayer = L.heatLayer(heatArray, {
+        radius: 20,
+        blur: 20,
+      }).addTo(myMap);
+    })
+    .catch(error => {
+      console.error("Error retrieving crime data:", error);
+    });
+}
+
+// No changes in the getLocationCoordinates() function
+function getLocationCoordinates(locationString) {
+  let regex = /\((-?\d+\.\d+),\s*(-?\d+\.\d+)\)/;
+  let match = regex.exec(locationString);
+  if (match && match.length === 3) {
+    let lat = parseFloat(match[1]);
+    let lng = parseFloat(match[2]);
+    if (!isNaN(lat) && !isNaN(lng)) {
+      return [lat, lng];
     }
-    return null;
   }
-  
+  return null;
+}
--- app.py
+++ app.py
@@ -11,15 +11,15 @@ from flask import Flask, jsonify, render_template
 #################################################
 # Database Setup
 #################################################
-# engine = create_engine("sqlite:///static/crimes_2023db.db")
+engine = create_engine("sqlite:///static/data/crimes_2023db.db")
 
-# # reflect an existing database into a new model
-# Base = automap_base()
-# # reflect the tables
-# Base.prepare(engine, reflect=True)
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
 
-# # Save reference to the table
-# Crimes = Base.classes.Clear_Crimes_2023
+# Save reference to the table
+Crimes = Base.classes.Clear_Crimes_2023
 
 #################################################
 # Flask Setup
@@ -36,25 +36,45 @@ def welcome():
     return render_template("index.html")
 
 
-# @app.route("/api/v1.0/dropdown")
-# def names():
-#     # Create our session (link) from Python to the DB
-#     session = Session(engine)
+@app.route("/api/v1.0/crime_heatmap_dropdown")
+def names():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
 
-#     """Return a list of all suicides by country"""
-#     # Query all suicide data
-#     # results = session.query(Crimes.ID, Crimes.LocationDescription, Crimes.PrimaryType).all()
-#     results = session.query(Crimes.LocationDescription).distinct(Crimes.LocationDescription).order_by(Crimes.LocationDescription.asc()).all()
-#     print(results)
-#     session.close()
+    # Return a list of
+    # Query all crime data data
+    # results = session.query(Crimes.ID, Crimes.PrimaryType, Crimes.Latitude, Crimes.Longitude).all()
+    results = session.query(Crimes.PrimaryType).distinct(Crimes.PrimaryType).order_by(Crimes.PrimaryType.asc()).all()
+    print(results)
+    session.close()
     
-#     # Convert list of tuples into normal list
-#     all_crimes = []
-#     for LocationDescription in results:
+    # Convert list of tuples into normal list
+    all_crimes = []
+    for PrimaryType in results:
         
-#         all_crimes.append(LocationDescription[0])
+        all_crimes.append(PrimaryType[0])
 
-#     return jsonify(all_crimes)
+    return jsonify(all_crimes)
+
+@app.route("/api/v1.0/chicago_crime_heatmap")
+def others():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    # Query all crime data
+    # results = session.query(Crimes.ID, Crimes.PrimaryType, Crimes.Latitude, Crimes.Longitude).all()
+    results = session.query(Crimes.PrimaryType, Crimes.Latitude, Crimes.Longitude).order_by(Crimes.PrimaryType.asc())
+    print(results)
+    session.close()
+    # Convert list of tuples into normal list
+    all_crimes = []
+    for PrimaryType, Latitude, Longitude in results:
+        crimes_dict = {}
+        #crimes_dict["ID"] = ID
+        crimes_dict["PrimaryType"] = PrimaryType
+        crimes_dict["Latitude"] = Latitude
+        crimes_dict["Longitude"] = Longitude
+        all_crimes.append(crimes_dict)
+    return jsonify(all_crimes)
 
 if __name__ == '__main__':
     app.run(debug=True)
